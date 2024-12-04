c1d6202a8e1f11afd15d158ffcad699c9f0a4447
Ryan Berube <rberube@intelligentsensinglabs.com>
2024-12-04 15:29:35 -0500

    all basic funcs bound

--- simulate/bindings.py
+++ simulate/bindings.py
@@ -5,12 +5,21 @@ from _ctypes import LoadLibrary
 handle = LoadLibrary("./simulate/kinecore.dll")
 lib = ctypes.WinDLL("./simulate/kinecore.dll", handle=handle)
 
-print(dir(lib))
+class leg_deltas(ctypes.Structure):
+    _fields_ = [
+        ("array1", c_float * 4),
+        ("array2", c_float * 4)
+    ]
+
+lib.test_function.restype = c_int
 
 lib.setup.restype = None
 lib.setup.argtypes = [c_float]
 
-lib.test_function.restype = c_int
+lib.set_real_thetas.restype = None
+lib.set_real_thetas.argtypes = [c_int, c_float, c_float]
+
+lib.get_deltas.restype = leg_deltas
 
 if __name__ == "__main__":
     print(f"Binding validate: {lib.test_function()}")
