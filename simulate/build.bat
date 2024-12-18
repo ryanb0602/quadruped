2e7b16eda9f56f138f92783d8c775b72ef90305b
Ryan Berube <rberube@intelligentsensinglabs.com>
2024-12-17 23:26:14 -0500

    modified build.bat to work with the new folder structure

--- simulate/build.bat
+++ simulate/build.bat
@@ -1,2 +1,22 @@
-::compile all simulate files
-g++ -I"C:\Users\Ryan Berube\Documents\Quadruped\include" -fPIC -shared -m64 -o kinecore.dll "C:\Users\Ryan Berube\Documents\Quadruped\lib\*.cpp" simlink.cpp
+@echo off
+setlocal enabledelayedexpansion
+
+:: Create the flattened directory if it doesn't exist
+if not exist "C:\Users\Ryan Berube\Documents\Quadruped\simulate\__TEMP_BUILD_DIR" (
+    mkdir "C:\Users\Ryan Berube\Documents\Quadruped\simulate\__TEMP_BUILD_DIR"
+)
+
+:: Copy all .cpp and .h files from nested folders to the flattened directory, excluding files with unwanted dependencies
+for /r "C:\Users\Ryan Berube\Documents\Quadruped\lib" %%f in (*.cpp *.h) do (
+    findstr /C:"#include <Arduino.h>" "%%f" >nul
+    if errorlevel 1 (
+        copy "%%f" "C:\Users\Ryan Berube\Documents\Quadruped\simulate\__TEMP_BUILD_DIR"
+    )
+)
+
+:: Compile the necessary files
+g++ -I"C:\Users\Ryan Berube\Documents\Quadruped\simulate\__TEMP_BUILD_DIR" -fPIC -shared -m64 -o kinecore.dll "C:\Users\Ryan Berube\Documents\Quadruped\simulate\__TEMP_BUILD_DIR\*.cpp" simlink.cpp
+
+rmdir /s /q "C:\Users\Ryan Berube\Documents\Quadruped\simulate\__TEMP_BUILD_DIR"
+
+endlocal
